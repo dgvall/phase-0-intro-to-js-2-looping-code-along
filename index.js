fbb4bfaf11cd64457d7881d77beb64d6151689e8
Diego Valles <dgvall134@gmail.com>
2022-09-04 15:55:18 -0600

    commit tree 83a199b1b6739804109ad417fd6f8096b4ba0f90

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let myNewArray = []
+      for ( let i = 0; i < array.length; i++) {
+        myNewArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+      }
+    return myNewArray
+  }
+
+  function countDown(posNum) {
+    console.log(posNum);
+    while(posNum > 0) {
+        posNum--
+        console.log(posNum)
+    }
+  }
